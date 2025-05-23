cdae35d6c70c473d6f2c152f8e10813ce958ee05
reokkamy <quinate@kakao.com>
2025-05-23 14:36:51 +0900

    5/23 nosql

---
+++ NOSQL.mongodb.js
@@ -0,0 +1,57 @@
+use('test')//기본 데이터 베이스, test 사용함. 생략시 기본test데이터베이스 사용
+
+//테이블 생성후 데이터 추가하는 기본문법:insertOne
+
+//db.[테이블명].insertOne({
+//   [컬럼명]:[값],
+//    name:'홍길동',
+//    age:20
+//   favorite: ['apple', 'banana'], })
+
+//한줄 실행: ctrl+alt+s
+//전체실행 : ctrl+alt+r
+db.user.insertOne({
+    name: '홍길동',
+    age: 20,
+    favorite: ['apple', 'banana'],
+})
+
+//조회
+//db.[테이블명].find({조건})
+db.user.find();
+
+//수정 
+// db.[테이블명].updateOne({조건}, {수정할 값})
+db.users.updateOne(
+    { name: '홍길동' }, // 조건
+    { $set: { age: 30 } } // 수정할 값
+)   
+
+// 삭제
+// db.[테이블명].deleteOne({조건})  
+db.users.deleteOne({ name: '홍길동' }) // 조건에 맞는 첫 번째 문서 삭제
+
+
+//capped collection, 컬렉션=테이블
+//컬렉션이 용량 초과하게되면, 오래된 데이터부터 차례대로 삭제하는 기능
+// db.createCollection('컬렉션명', { capped: true, size: 용량 })
+//용량이 5kb인 컬렉션 생성, 부가 기능으로 용량초과시 오래된 데이터부터 삭제
+db.createCollection('logs', { capped: true, size: 5000}) //5kb
+//샘플데이터 추가 , 반복문을 이요해서 샘플로 1000개 추가
+for (let i = 2000; i < 3000; i++) {
+    db.logs.insertOne({
+       message: `로그 메시지 ${i}`,
+         timestamp: new Date(),//오라클에서 sysdate와 같은 기능
+    })
+}
+db.logs.find() //컬렉션에 있는 모든 데이터 조회
+
+db.createCollection('logs2', { capped: true, size: 5000})
+
+for (let i = 1000; i < 2000; i++) {
+    db.logs2.insertOne({
+       message: `로그 메시지 ${i}`,
+         timestamp: new Date(),//오라클에서 sysdate와 같은 기능
+    })
+}
+
